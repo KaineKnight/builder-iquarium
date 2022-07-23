40a66cb73e0966075441b65081e4f4777a642da8
lokerd <lokerd5110@yandex.ru>
2022-07-23 04:50:46 +0300

    Fix

--- React/builder-app/src/View/Components/Tasks.js
+++ React/builder-app/src/View/Components/Tasks.js
@@ -3,6 +3,7 @@ import light from '../../Styles/Images/Tasks/lightning-icon.svg';
 import clock from '../../Styles/Images/Tasks/clock-icon.svg';
 import brock from '../../Styles/Images/Tasks/brock-icon.svg';
 import exclaim from '../../Styles/Images/Tasks/exclaim-icon.svg';
+import TaskCard from './TaskCard';
 
 function Tasks() {
     return (
@@ -34,27 +35,27 @@ function Tasks() {
                 <li className='section'>
                     <h2>Выполнить</h2>
                     <ul className='section-items'>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li><TaskCard/></li>
+                        <li><TaskCard/></li>
+                        <li><TaskCard/></li>
                     </ul>
                 </li>
                 <li className='line'></li>
                 <li className='section'>
                     <h2>В процессе</h2>
                     <ul className='section-items'>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li><TaskCard/></li>
+                        <li><TaskCard/></li>
+                        <li><TaskCard/></li>
                     </ul>
                 </li>
                 <li className='line'></li>
                 <li className='section'>
                     <h2>Завершено</h2>
                     <ul className='section-items'>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li><TaskCard/></li>
+                        <li><TaskCard/></li>
+                        <li><TaskCard/></li>
                     </ul>
                 </li>
                 <li className='line'></li>
